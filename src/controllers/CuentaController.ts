1408cb587a520a7c48ebb5724bf7fd333ee5b0c9
= <=>
2023-08-27 20:51:14 -0400

    avances

--- src/controllers/CuentaController.ts
+++ src/controllers/CuentaController.ts
@@ -26,10 +26,49 @@ export const getCuentas = async (req : Request, res : Response)=>{
     res.json(cuentas);
 }
 export const postNew = async (req : Request, res : Response)=>{
-
+    const { body } = req;
+    try {
+        const existe = await Cuenta.findOne({
+            where:{
+                usuario: body.usuario,
+                nombre: body.nombre
+            }
+        });
+        if(existe){
+            res.status(400).json({
+                error: 'El usuario ya tiene una cuenta con ese nombre.'
+            });
+            return;
+        }
+        const cuenta = await Cuenta.build(body);
+        cuenta.save();
+        res.json({id:cuenta});
+    } catch (error) {
+        res.status(500).json({
+            error: 'Hubo un problema.'
+        });
+    }
 }
 export const patchActualizar = async (req : Request, res : Response)=>{
-
+    const { id } = req.params;
+    const { body } = req;
+    try{
+        const cuenta = await Cuenta.findByPk(id);
+        if(cuenta){
+            
+            res.json(cuenta);
+            return;
+        }
+        res.status(400).json({
+            error: 'La cuenta no existe.'
+        });
+        return;
+    }catch(error){
+        console.log(error);
+        res.status(500).json({
+            error: 'Hubo un problema.'
+        });
+    }
 }
 export const deleteCuenta = async (req : Request, res : Response)=>{
 
